d2b11f80888ad9e07637c0967f8f24e1c5414c3d
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-04-16 21:56:44 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,42 +1,58 @@
 import sys
 
-BASE = 263
-PRIME = 1000000007
 
 def read_input():
     return (input().rstrip(), input().rstrip())
 
+
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def hash(text):
-    h = 0
-    for c in reversed(text):
-        h = (h * BASE + ord(c)) % PRIME
+
+def precompute_hashes(text, p_len, p, x):
+    h = [0] * (len(text) - p_len + 1)
+    s = text[len(text) - p_len:]
+    h[-1] = poly_hash(s, p, x)
+    y = 1
+    for i in range(p_len):
+        y = (y * x) % p
+    for i in range(len(text) - p_len - 1, -1, -1):
+        pre_hash = x * h[i+1] + ord(text[i]) - y * ord(text[i+p_len])
+        while pre_hash < 0:
+            pre_hash += p
+        h[i] = pre_hash % p
     return h
 
-def precompute_hashes(text, pattern_len):
-    t = len(text)
-    h = [None] * (t - pattern_len + 1)
-    s = text[t - pattern_len:]
-    h[t - pattern_len] = hash(s)
-    y = 1
-    for i in range(pattern_len):
-        y = (y * BASE) % PRIME
-    for i in range(t - pattern_len - 1, -1, -1):
-        pre_hash = BASE * h[i+1] + ord(text[i]) - y * ord(text[i+pattern_len])
-        h[i] = pre_hash % PRIME
+
+def poly_hash(s, p, x):
+    h = 0
+    for i in reversed(s):
+        h = (h * x + ord(i)) % p
     return h
+
+
+def update_hash(h, old_c, new_c, p, x, m):
+    new_h = ((h - ord(old_c) * m) * x + ord(new_c)) % p
+    return new_h
+
 
 def get_occurrences(pattern, text):
+    PRIME = 10 ** 9 + 7
+    x = 1
+    p_len = len(pattern)
+    t_len = len(text)
+    for i in range(p_len):
+        x = (x * 263) % PRIME
+    p_hash = poly_hash(pattern, PRIME, x)
+    h = precompute_hashes(text, p_len, PRIME, x)
     occurrences = []
-    p_hash = hash(pattern)
-    t_len, p_len = len(text), len(pattern)
-    h = precompute_hashes(text, p_len)
     for i in range(t_len - p_len + 1):
-        if p_hash == h[i] and text[i:i+p_len] == pattern:
+        if p_hash != h[i]:
+            continue
+        if text[i:i + p_len] == pattern:
             occurrences.append(i)
     return occurrences
+
 
 if __name__ == '__main__':
     pattern, text = read_input()
