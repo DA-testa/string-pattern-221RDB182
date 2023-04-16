c93ce9b628ebf6b85c78b48b35d4e97225b1a19c
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-04-16 21:45:49 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,16 +1,13 @@
-import random
-
-PRIME = 10**9 + 7
-BASE = random.randint(1, PRIME-1)
-
+PRIME = 1000000007
+BASE = 31
 
 def read_input():
-    return (input().rstrip(), input().rstrip())
-
+    pattern = input().rstrip()
+    text = input().rstrip()
+    return pattern, text
 
 def print_occurrences(output):
-    print(' '.join(map(str, output)))
-
+    print(" ".join(map(str, output)))
 
 def hash(s):
     h = 0
@@ -18,22 +15,25 @@ def hash(s):
         h = (h * BASE + ord(c)) % PRIME
     return h
 
+def update_hash(h, c1, c2_pow):
+    h = (h - ord(c1) * c2_pow) % PRIME
+    h = (h * BASE) % PRIME
+    return h
 
 def get_occurrences(pattern, text):
-    p_len, t_len = len(pattern), len(text)
+    n = len(text)
+    m = len(pattern)
     pattern_hash = hash(pattern)
-    text_hashes = [None] * (t_len - p_len + 1)
-    text_hashes[-1] = hash(text[t_len-p_len:t_len])
-    POW_BASE = 1
-    for i in range(p_len):
-        POW_BASE = (POW_BASE * BASE) % PRIME
-    for i in range(t_len-p_len-1, -1, -1):
-        text_hashes[i] = (text_hashes[i+1] * BASE + ord(text[i]) - POW_BASE * ord(text[i+p_len])) % PRIME
-    return [i for i in range(t_len-p_len+1) if text_hashes[i] == pattern_hash]
-
+    c2_pow = pow(BASE, m-1, PRIME)
+    window_hash = hash(text[n-m:])
+    occurrences = []
+    for i in range(n-m, -1, -1):
+        if window_hash == pattern_hash and text[i:i+m] == pattern:
+            occurrences.append(i)
+        window_hash = update_hash(window_hash, text[i], text[i+m] * c2_pow % PRIME)
+    return reversed(occurrences)
 
 if __name__ == '__main__':
     pattern, text = read_input()
     occurrences = get_occurrences(pattern, text)
     print_occurrences(occurrences)
-    
