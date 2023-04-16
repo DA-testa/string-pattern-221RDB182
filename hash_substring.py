d901a6f1e7542fc54165e8c5c3dc54ca255844d0
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-04-16 22:58:35 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,19 +1,22 @@
 import sys
 
+
 def read_input():
     try:
+        choice = input()
         pattern = input().rstrip()
         text = input().rstrip()
     except EOFError:
-        print("Error: End of file reached while reading input.")
-        sys.exit(1)
+        return None
     return pattern, text
+
 
 def poly_hash(s, p, x):
     h = 0
     for i in range(len(s) - 1, -1, -1):
         h = (h * x + ord(s[i])) % p
     return h
+
 
 def precompute_hashes(T, P, p, x):
     if len(T) == len(P):
@@ -28,6 +31,7 @@ def precompute_hashes(T, P, p, x):
         H[i] = (x * H[i + 1] + ord(T[i]) - y * ord(T[i + len(P)])) % p
     return H
 
+
 def rabin_karp(pattern, text):
     p = 10**9 + 7
     x = 263
@@ -41,12 +45,35 @@ def rabin_karp(pattern, text):
             result.append(i)
     return result
 
+
+def print_occurrences(output):
+    print(' '.join(map(str, output)))
+
+
+def get_occurrences(input_vers, pattern, text):
+    if input_vers == "I":
+        return rabin_karp(pattern, text)
+    elif input_vers == "F":
+        text_len = len(text)
+        pattern_len = len(pattern)
+        occurrences = []
+        pattern_hash = 0
+        text_hash = 0
+        for i in range(pattern_len):
+            pattern_hash += ord(pattern[i]) * pow(10, pattern_len - i - 1)
+            text_hash += ord(text[i]) * pow(10, pattern_len - i - 1)
+        for i in range(text_len - pattern_len + 1):
+            if text_hash == pattern_hash:
+                if text[i:i + pattern_len] == pattern:
+                    occurrences.append(i)
+            if i < text_len - pattern_len:
+                text_hash = (text_hash - ord(text[i]) * pow(10, pattern_len - 1)) * 10 + ord(text[i + pattern_len])
+        return occurrences
+    else:
+        print("try again")
+        exit()
+
+
 if __name__ == '__main__':
-    while True:
-        input_data = read_input()
-        if input_data is None:
-            break
-        pattern, text = input_data
-        result = rabin_karp(pattern, text)
-        for pos in result:
-            print(pos, end=' ')
+    input_vers, pattern, text = read_input()
+    print_occurrences(get_occurrences(input_vers, pattern, text)))
