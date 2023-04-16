33223792328c77a0f7fcc40e1efe09bd6adee95c
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-04-16 23:55:53 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -67,8 +67,8 @@ def get_occurrences(input_vers, pattern, text):
         power = 1
         occurrences = []
         for i in range(pattern_len):
-            pattern_hash = (pattern_hash * 10 + ord(pattern[i])) % prime
-            text_hash = (text_hash * 10 + ord(text[i])) % prime
+            pattern_hash = (pattern_hash + ord(pattern[i]) * power) % prime
+            text_hash = (text_hash + ord(text[i]) * power) % prime
             power = (power * 10) % prime
         for i in range(text_len - pattern_len + 1):
             if pattern_hash == text_hash:
@@ -83,9 +83,8 @@ def get_occurrences(input_vers, pattern, text):
 
 if __name__ == '__main__':
     try:
-        with open('tests/06') as f:
-            input_vers, pattern, text = f.readlines()
-        print_occurrences(get_occurrences(input_vers.strip(), pattern.strip(), text.strip()))
+        input_vers, pattern, text = read_input()
+        print_occurrences(get_occurrences(input_vers, pattern, text))
     except EOFError:
         print("Error: Input was not provided")
     finally:
