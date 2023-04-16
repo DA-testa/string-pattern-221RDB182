4494705f2352b8100fec54c2aeb97a793d634367
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-04-16 21:43:35 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,14 +1,16 @@
-PRIME = 10**9+7
-BASE = 263
+import random
+
+PRIME = 10**9 + 7
+BASE = random.randint(1, PRIME-1)
+
 
 def read_input():
-    try:
-        pattern = input().rstrip()
-        text = input().rstrip()
-        return pattern, text
-    
-    def print_occurrences(output):
-    print(" ".join(map(str, output)))
+    return (input().rstrip(), input().rstrip())
+
+
+def print_occurrences(output):
+    print(' '.join(map(str, output)))
+
 
 def hash(s):
     h = 0
@@ -16,24 +18,22 @@ def hash(s):
         h = (h * BASE + ord(c)) % PRIME
     return h
 
-def update_hash(h, old_c, new_c, length):
-    h = (h - ord(old_c) * pow(BASE, length-1, PRIME)) % PRIME
-    h = (h * BASE + ord(new_c)) % PRIME
-    return h
 
 def get_occurrences(pattern, text):
-    occurrences = []
+    p_len, t_len = len(pattern), len(text)
     pattern_hash = hash(pattern)
-    window_hash = hash(text[-len(pattern):])
-    length = len(pattern)
-    if window_hash == pattern_hash and text[-length:] == pattern:
-        occurrences.append(len(text)-length)
+    text_hashes = [None] * (t_len - p_len + 1)
+    text_hashes[-1] = hash(text[t_len-p_len:t_len])
+    POW_BASE = 1
+    for i in range(p_len):
+        POW_BASE = (POW_BASE * BASE) % PRIME
+    for i in range(t_len-p_len-1, -1, -1):
+        text_hashes[i] = (text_hashes[i+1] * BASE + ord(text[i]) - POW_BASE * ord(text[i+p_len])) % PRIME
+    return [i for i in range(t_len-p_len+1) if text_hashes[i] == pattern_hash]
 
-    for i in range(len(text)-length-1, -1, -1):
-        window_hash = update_hash(window_hash, text[i+length], text[i], length)
-        if window_hash == pattern_hash and text[i:i+length] == pattern:
-            occurrences.append(i)
-    return reversed(occurrences)
 
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    pattern, text = read_input()
+    occurrences = get_occurrences(pattern, text)
+    print_occurrences(occurrences)
+    
